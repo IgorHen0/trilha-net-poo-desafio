e8b56ab799a4c359303cc4473af1033e9e7e0bc0
IgorHen0 <igorhenrique0472@gmail.com>
2024-10-26 13:33:12 -0300

    resolução do desafio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Iphone iphone = new Iphone("(31) 98716-0074", "Iphone 12", "123456789", 128);
+Nokia nokia = new Nokia("(31) 98716-0074", "Nokia 3310", "987654321", 64);
+
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("WhatsApp");
+
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Facebook");
